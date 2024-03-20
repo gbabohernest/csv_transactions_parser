73cf7ce49f68ba86dfda833dab9c90b232acff37
gbabohernest <gbaboh.ernest@gmail.com>
2024-03-20 05:33:34 +0300

    define constants for different paths, get the path to the root directory of the current directory, test the transactions functions

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+
+require APP_PATH . "App.php";
+
+$transactionsFiles = getCsvTransactionFiles(FILES_PATH);
+//var_dump($transactionsFiles);
+
+
+$allTransactions = [];
+
+foreach ($transactionsFiles as $file) {
+    $allTransactions = array_merge($allTransactions, readTransactionsFromCsvFiles($file));
+}
+
+//print_r($allTransactions);
